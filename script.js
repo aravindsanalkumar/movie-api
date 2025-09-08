757fd4bdf777a21ac974575f06983b76f2e5c938
aravindsanalkumar <aravindsanalkumar@gmail.com>
2025-09-08 13:52:30 +0530

    movie api created

---
+++ script.js
@@ -0,0 +1,57 @@
+//http://www.omdbapi.com/?i=tt3896198&apikey=50a89dd
+
+
+const apikey = '50a89dd';
+
+
+window.onload = function () {
+    getMovieData('Inception');
+}
+
+function searchMovie() {
+    const searchMovieName = document.getElementById('searchMovieName').value;
+    if (searchMovieName != "") {
+        getMovieData(searchMovieName);
+    }
+}
+
+function getMovieData(movieName) {
+    fetch(`http://www.omdbapi.com/?t=${movieName}&apikey=${apikey}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            const cardMovie = document.getElementById('cardMovie');
+            if (data.Response == 'False') {
+                console.log('movie not found')
+                cardMovie.innerHTML = ``
+            }
+            else {
+                cardMovie.innerHTML = `<img src="${data.Poster}" alt="">`;
+                const movieDetails = document.getElementById('movieDetails');
+                movieDetails.innerHTML = `
+                     <div class="d-flex gap-3 align-items-center">
+                        <p>Movie name :</p>
+                        <p>${data.Title}</p>
+                    </div>
+                    <div class="d-flex gap-3 align-items-center">
+                        <p>Actors :</p>
+                        <p>${data.Actors}</p>
+                    </div>
+                    <div class="d-flex gap-3 align-items-center">
+                        <p>Director :</p>
+                        <p>${data.Director}</p>
+                    </div>
+                    <div class="d-flex gap-3 align-items-center">
+                        <p>Box Office :</p>
+                        <p>${data.BoxOffice}</p>
+                    </div>
+                `
+
+            }
+            document.getElementById('searchMovieName').value = "";
+        });
+}
+
+
+
+
